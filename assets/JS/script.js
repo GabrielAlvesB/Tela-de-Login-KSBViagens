649bdc60c1043140d86bafe1f785f4b69e33fbf6
Gabriel Alves <75049066+GabrielAlvesB@users.noreply.github.com>
2023-06-21 10:26:56 -0300

    Ajustes pequenos

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,20 +1,3 @@
-const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
-
-  alertPlaceholder.append(wrapper)
+function loginOk() {
+  alert("Cadastro feito com sucesso!")
 }
-
-const alertTrigger = document.getElementById('liveAlertBtn')
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Nice, you triggered this alert message!', 'success')
-  })
-}
-//Tela de criar usuario
